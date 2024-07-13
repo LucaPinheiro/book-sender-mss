89a5145d45c5b8e7ea54a4964cafa0ab12c51b19
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 01:16:58 -0300

    Refactor IUserRepository to add sessionUser method

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -3,5 +3,6 @@ import { User } from "../entities/user";
 export interface IUserRepository {
   createUser(user: User): Promise<User>;
   getUserByEmail(email: string): Promise<User | undefined>
+  sessionUser(email: string, password: string): Promise<User | undefined>
   getUserById(id: string): Promise<User | undefined>
 }
