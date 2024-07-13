699fc978d910ff6932cbbd2ba06be7ed6ece65a7
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 03:45:13 -0300

    refactor: Rename GetAllEmailsUsecase to GetAllEmailsByTeamUsecase and update execute method to accept team parameter

--- src/modules/get_all_emails_by_team/app/get_all_emails_by_team_usecase.ts
+++ src/modules/get_all_emails_by_team/app/get_all_emails_by_team_usecase.ts
@@ -1,10 +1,10 @@
 import { Email } from "../../../shared/domain/entities/email";
 import { IEmailRepository } from "../../../shared/domain/repositories/email_repository_interface";
 
-export class GetAllEmailsUsecase {
+export class GetAllEmailsByTeamUsecase {
   constructor(private repo: IEmailRepository) {}
 
-  async execute(): Promise<Email[]> {
-    return this.repo.getAllEmails();
+  async execute(team: string): Promise<Email[]> {
+    return this.repo.getAllEmailsByTeam(team);
   }
 }
