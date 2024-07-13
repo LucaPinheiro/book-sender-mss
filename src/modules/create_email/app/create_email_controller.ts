67a4c68ec55fbfb79dabe8a1f393192a0d1ac6e6
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 02:53:49 -0300

    feat: Add CreateEmailController for creating email entities

---
+++ src/modules/create_email/app/create_email_controller.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import {
+  InvalidParameter,
+  InvalidRequest,
+  MissingParameters,
+} from "../../../shared/helpers/errors/controller_errors";
+import { EntityError } from "../../../shared/helpers/errors/domain_errors";
+import {
+  BadRequest,
+  Conflict,
+  Forbidden,
+  InternalServerError,
+  ParameterError,
+} from "../../../shared/helpers/http/http_codes";
+import { CreateEmailUsecase } from "./create_email_usecase";
+import { EmailProps } from "../../../shared/domain/entities/email";
+import { CreateEmailViewModel } from "./create_email_viewmodel";
+import { DuplicatedItem } from "../../../shared/helpers/errors/usecase_errors";
+
+export class CreateEmailController {
+  constructor(private createUserUsecase: CreateEmailUsecase) {}
+
+  async createEmail(req: Request, res: Response) {
+    try {
+      console.log("TENTANDO CRIAR USUÁRIO");
+      const { email, team, role } = req.body;
+
+      const errors: MissingParameters[] = [];
+
+      if (!email) {
+        errors.push(new MissingParameters("Email"));
+      }
+
+      if (!team) {
+        errors.push(new MissingParameters("Team"));
+      }
+
+      if (!team) {
+        errors.push(new MissingParameters("Role"));
+      }
+
+      if (errors.length > 0) {
+        return res.status(400).json(errors);
+      }
+
+      const userProps: EmailProps = {
+        email,
+        team,
+        role,
+      };
+
+      await this.createUserUsecase.execute(userProps);
+
+      const viewModel = new CreateEmailViewModel(
+        "Email cadastrado com sucesso!"
+      );
+      res.status(201).json(viewModel);
+    } catch (error: any) {
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message).send(res);
+      }
+      if (error instanceof InvalidParameter) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof EntityError) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof Forbidden) {
+        return new Forbidden(error.getMessage()).send(res);
+      }
+      if (error instanceof DuplicatedItem) {
+        return new Conflict(error.message).send(res);
+      }
+      return new InternalServerError("Internal Server Error").send(res);
+    }
+  }
+}
