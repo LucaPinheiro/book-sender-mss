d9ce9e68019bd02efe511162463b6cd42d1e186a
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 04:02:40 -0300

    refactor: Update server.ts to use port 3030 instead of 3000

--- server.ts
+++ server.ts
@@ -1,11 +1,10 @@
 import "dotenv/config";
 import app from "./src/app";
-import routes from "./src/routes/routes";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT} 🚀`);
 });
 
 import cors from "cors";
