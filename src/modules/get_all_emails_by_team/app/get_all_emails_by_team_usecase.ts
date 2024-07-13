800f5f2954af135dacad981ebdc8cad944214232
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 03:48:09 -0300

    refactor: Add validation for team parameter in GetAllEmailsByTeamUsecase

--- src/modules/get_all_emails_by_team/app/get_all_emails_by_team_usecase.ts
+++ src/modules/get_all_emails_by_team/app/get_all_emails_by_team_usecase.ts
@@ -1,10 +1,16 @@
 import { Email } from "../../../shared/domain/entities/email";
+import { TEAM } from "../../../shared/domain/enums/team_enum";
 import { IEmailRepository } from "../../../shared/domain/repositories/email_repository_interface";
+import { InvalidParameter } from "../../../shared/helpers/errors/controller_errors";
 
 export class GetAllEmailsByTeamUsecase {
   constructor(private repo: IEmailRepository) {}
 
   async execute(team: string): Promise<Email[]> {
+    if (!Email.isValidTeam(team as TEAM)) {
+      throw new InvalidParameter("team", "Invalid team name");
+    }
+    
     return this.repo.getAllEmailsByTeam(team);
   }
 }
