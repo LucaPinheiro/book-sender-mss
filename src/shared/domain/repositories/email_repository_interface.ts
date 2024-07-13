ff1dd24821dc036c51fd52171e4591674c6e7af9
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 02:26:38 -0300

    feat: Update EmailRepositoryInterface to include team and role parameters in create_email method

--- src/shared/domain/repositories/email_repository_interface.ts
+++ src/shared/domain/repositories/email_repository_interface.ts
@@ -1,5 +1,5 @@
 import { Email } from "../entities/email";
 
 export interface IEmailRepositoryInterface {
-    create_email(email: string): Promise<Email>;
+  create_email(email: string, team: string, role: string): Promise<Email>;
 }
