375ab11b52d67560a2868238abe685e389dce7a8
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 03:11:10 -0300

    feat: Add validation for email properties in CreateEmailUsecase

--- src/modules/create_email/app/create_email_usecase.ts
+++ src/modules/create_email/app/create_email_usecase.ts
@@ -1,6 +1,7 @@
 import { EntityError } from "../../../shared/helpers/errors/domain_errors";
 import { IEmailRepository } from "../../../shared/domain/repositories/email_repository_interface";
 import { Email, EmailProps } from "../../../shared/domain/entities/email";
+import { InvalidParameter } from "../../../shared/helpers/errors/controller_errors";
 
 export class CreateEmailUsecase {
   constructor(private repo: IEmailRepository) {}
@@ -12,6 +13,18 @@ export class CreateEmailUsecase {
 
     if (!emailProps.role) {
       throw new EntityError("role");
+    }
+
+    if (!Email.isValidRole(emailProps.role)) {
+      throw new InvalidParameter("role", "Invalid role");
+    }
+
+    if (!Email.isValidEmail(emailProps.email)) {
+      throw new InvalidParameter("email", "Invalid email format");
+    }
+
+    if (!Email.isValidTeam(emailProps.team)) {
+      throw new InvalidParameter("team", "Invalid team");
     }
 
     if (!emailProps.team) {
