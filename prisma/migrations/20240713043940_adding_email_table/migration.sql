182775b56bbed75f839edd7f6a84926ec0a0acd6
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 02:11:29 -0300

    feat: Add email table to the database schema

---
+++ prisma/migrations/20240713043940_adding_email_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Email" (
+    "email_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "team" TEXT NOT NULL,
+
+    CONSTRAINT "Email_pkey" PRIMARY KEY ("email_id")
+);
