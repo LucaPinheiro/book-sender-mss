37b4109a2633c6c83a2b8bd98bb54ef75d9ba043
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 02:54:00 -0300

    feat: Add createEmailPresenter to handle email creation route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,19 +2,24 @@ import { Express, Request, Response } from "express";
 import CreateUserPresenter from "../modules/create_user/app/create_user_presenter";
 import AuthUserPresenter from "../modules/auth_user/app/auth_user_presenter";
 import GetUserByIdPresenter from "../modules/get_user_by_id/app/get_user_by_id_presenter";
+import createEmailPresenter from "../modules/create_email/app/create_email_presenter";
 
 import app from "../app";
 
 const routes = (app: Express) => {
   app
     .route("/")
-    .get((req: Request, res: Response) => res.status(200).send("Api Book Sender"));
+    .get((req: Request, res: Response) =>
+      res.status(200).send("Api Book Sender")
+    );
 
-  
   // user routes
   app.use("/api", CreateUserPresenter);
   app.use("/api", AuthUserPresenter);
   app.use("/api", GetUserByIdPresenter);
-  }
-  
+
+  // email routes
+  app.use("/api", createEmailPresenter);
+};
+
 export default routes;
