0988f4b4544d9bce5ee89977534ac587b0ee04ba
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-13 02:11:33 -0300

    refactor: Add role validation to User class

--- src/shared/domain/entities/email.ts
+++ src/shared/domain/entities/email.ts
@@ -1,10 +1,12 @@
 import { EntityError } from "../../helpers/errors/domain_errors";
+import { ROLE } from "../enums/role_enum";
 import { TEAM } from "../enums/team_enum";
 
 export interface UserProps {
   emailId?: string;
   email: string;
   team: TEAM;
+  role: ROLE;
 }
 
 export class User {
@@ -19,6 +21,10 @@ export class User {
 
     if (!User.isValidTeam(props.team)) {
       throw new EntityError("Invalid team");
+    }
+
+    if (!User.isValidRole(props.role)) {
+      throw new EntityError("Invalid role");
     }
   }
 
@@ -44,5 +50,9 @@ export class User {
   static isValidEmail(email: string): boolean {
     const emailRegex = /\S+@santander\S+\.\S+/;
     return emailRegex.test(email);
+  }
+
+  static isValidRole(role: ROLE): boolean {
+    return Object.values(ROLE).includes(role);
   }
 }
