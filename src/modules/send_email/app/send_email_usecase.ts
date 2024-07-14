ed375a28a079de25c4fbb130b13998ce7407c322
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-14 04:35:21 -0300

    refactor: Update send email usecase to use email repository for retrieving email addresses

--- src/modules/send_email/app/send_email_usecase.ts
+++ src/modules/send_email/app/send_email_usecase.ts
@@ -1,13 +1,21 @@
+import { send } from "process";
+import { IEmailRepository } from "../../../shared/domain/repositories/email_repository_interface";
 import { sendEmails } from "../../../shared/infra/repositories/email_repository_nodemailer";
 export class SendEmailUsecase {
+  constructor(private emailRepository: IEmailRepository) {}
   async execute(
-    recipients: string[],
+    team: string,
     subject: string,
     text: string,
     pdfPath: string
   ): Promise<boolean> {
+    const emailsBoss = await this.emailRepository.getAllEmailsExceptTimeRole();
+    const emails = await this.emailRepository.getAllEmailsByTeam(team);
+    emailsBoss.push(...emails);
     try {
-      await sendEmails(recipients, subject, text, pdfPath);
+      console.log("OLHA AQUI CACETE ", emailsBoss);
+      const emailList = emails.map((email) => email.email);
+      await sendEmails(emailList, subject, text, pdfPath);
       return true;
     } catch (error) {
       console.error("Failed to send email:", error);
