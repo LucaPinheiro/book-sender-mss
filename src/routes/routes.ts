b82ec47d7c69414b8a84730f6a88ccb73b31f648
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-14 01:14:12 -0300

    refactor: Add route for retrieving all emails by role

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,7 @@ import GetUserByIdPresenter from "../modules/get_user_by_id/app/get_user_by_id_p
 import createEmailPresenter from "../modules/create_email/app/create_email_presenter";
 import getAllEmailsPresenter from "../modules/get_all_emails/app/get_all_emails_presenter";
 import getAllEmailsByTeamPresenter from "../modules/get_all_emails_by_team/app/get_all_emails_by_team_presenter";
+import getAllEmailsByRolePresenter from "../modules/get_all_emails_by_role/app/get_all_emails_by_role_presenter";
 
 import app from "../app";
 
@@ -24,6 +25,7 @@ const routes = (app: Express) => {
   app.use("/api", createEmailPresenter);
   app.use("/api", getAllEmailsPresenter);
   app.use("/api", getAllEmailsByTeamPresenter);
+  app.use("/api", getAllEmailsByRolePresenter);
 };
 
 export default routes;
