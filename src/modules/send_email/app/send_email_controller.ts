264bd9f82824f184aafb378d28f440305b963b1b
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-14 02:04:31 -0300

    refactor: Add SendEmailController for handling email sending functionality

---
+++ src/modules/send_email/app/send_email_controller.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express";
+import { SendEmailUsecase } from "./send_email_usecase";
+import {
+  InvalidParameter,
+  InvalidRequest,
+} from "../../../shared/helpers/errors/controller_errors";
+import {
+  BadRequest,
+  Conflict,
+  Forbidden,
+  InternalServerError,
+  ParameterError,
+} from "../../../shared/helpers/http/http_codes";
+import { EntityError } from "../../../shared/helpers/errors/domain_errors";
+import { DuplicatedItem } from "../../../shared/helpers/errors/usecase_errors";
+import { SendEmailViewModel } from "./send_email_viewmodel";
+
+export class SendEmailController {
+  constructor(private usecase: SendEmailUsecase) {}
+
+  async handle(req: Request, res: Response) {
+    try {
+      const { recipients, subject, text } = req.body;
+      const pdfPath = req.file?.path;
+
+      if (!pdfPath) {
+        throw new InvalidRequest("PDF file is required.");
+      }
+      if (!recipients) {
+        throw new InvalidRequest("Recipients are required.");
+      }
+      if (!subject) {
+        throw new InvalidRequest("Subject is required.");
+      }
+      if (!text) {
+        throw new InvalidRequest("Text is required.");
+      }
+
+      const success = await this.usecase.execute(
+        recipients,
+        subject,
+        text,
+        pdfPath
+      );
+      const viewmodel = new SendEmailViewModel("E-mails enviados com sucesso!");
+      return res.status(200).send(viewmodel);
+    } catch (error: any) {
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message).send(res);
+      }
+      if (error instanceof InvalidParameter) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof EntityError) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof Forbidden) {
+        return new Forbidden(error.getMessage()).send(res);
+      }
+      if (error instanceof DuplicatedItem) {
+        return new Conflict(error.message).send(res);
+      }
+      if (error instanceof BadRequest) {
+        return new BadRequest(error.getMessage()).send(res);
+      }
+      return new InternalServerError("Internal Server Error").send(res);
+    }
+  }
+}
