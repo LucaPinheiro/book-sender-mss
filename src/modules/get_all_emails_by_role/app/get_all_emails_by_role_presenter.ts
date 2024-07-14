d5b92b6e00d6ccd42c267ffa7c2afb6eb68115b4
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-14 01:00:19 -0300

    refactor: Add route for retrieving all emails by role

---
+++ src/modules/get_all_emails_by_role/app/get_all_emails_by_role_presenter.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { EmailRepositoryPrisma } from "../../../shared/infra/repositories/email_repository_prisma";
+import { GetAllEmailsByRoleController } from "./get_all_emails_by_role_controller";
+import { authenticateToken } from "../../../shared/middlewares/jwt_middleware";
+import { GetAllEmailsByRoleUsecase } from "./get_all_emails_by_role_usecase";
+
+const router = express.Router();
+
+const emailRepository = new EmailRepositoryPrisma();
+const getAllEmailsByRoleUsecase = new GetAllEmailsByRoleUsecase(emailRepository);
+const getAllEmailsByRoleController = new GetAllEmailsByRoleController(getAllEmailsByRoleUsecase);
+
+router.get(
+  "/emails/:role",
+  authenticateToken,
+  async (req: Request, res: Response) => {
+    await getAllEmailsByRoleController.handle(req, res);
+  }
+);
+
+export default router;
