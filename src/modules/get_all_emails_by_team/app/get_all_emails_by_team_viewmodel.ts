9db785f418848b3d8e2f03835776ebf9e13f61d4
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-14 02:46:39 -0300

    refactor: Update GetAllEmailsByTeamViewmodel to include role property

--- src/modules/get_all_emails_by_team/app/get_all_emails_by_team_viewmodel.ts
+++ src/modules/get_all_emails_by_team/app/get_all_emails_by_team_viewmodel.ts
@@ -2,8 +2,10 @@ import { Email } from "../../../shared/domain/entities/email";
 
 export class GetAllEmailsByTeamViewmodel {
   email: string;
+  role: string;
 
   constructor(email: Email) {
     this.email = email.email;
+    this.role = email.role;
   }
 }
