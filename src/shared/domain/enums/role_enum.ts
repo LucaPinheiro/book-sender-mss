5da9b7bd636f29bb5c4a3fc288017cc5c2673beb
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-14 02:54:45 -0300

    refactor: Update role_enum.ts to improve code readability and maintainability

--- src/shared/domain/enums/role_enum.ts
+++ src/shared/domain/enums/role_enum.ts
@@ -1,25 +1,24 @@
 export enum ROLE {
-    CHEFE_FINANCEIRA = "FINANCAS",
-    DIRETOR = "DIRETOR",
-    HEAD_FINANCAS = "HEAD",
-    GESTOR = "GESTOR",
-    TIME = "TIME",
+  CHEFE_FINANCEIRA = "FINANCAS",
+  DIRETOR = "DIRETOR",
+  HEAD_FINANCAS = "HEAD",
+  GESTOR = "GESTOR",
+  TIME = "TIME",
+}
+
+export function toEnum(value: string): ROLE {
+  switch (value) {
+    case "FINANCAS":
+      return ROLE.CHEFE_FINANCEIRA;
+    case "DIRETOR":
+      return ROLE.DIRETOR;
+    case "HEAD":
+      return ROLE.HEAD_FINANCAS;
+    case "GESTOR":
+      return ROLE.GESTOR;
+    case "TIME":
+      return ROLE.TIME;
+    default:
+      throw new Error("Invalid value");
   }
-  
-  export function toEnum(value: string): ROLE {
-    switch (value) {
-      case "FINANCAS":
-        return ROLE.CHEFE_FINANCEIRA;
-      case "DIRETOR":
-        return ROLE.DIRETOR;
-      case "HEAD":
-        return ROLE.HEAD_FINANCAS;
-      case "GESTOR":
-        return ROLE.GESTOR;
-      case "TIME":
-        return ROLE.TIME;
-      default:
-        throw new Error("Invalid value");
-    }
-  }
-  
+}
